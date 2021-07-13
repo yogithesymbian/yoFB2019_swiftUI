5e76f91a4f326f4ca754eebb5a02711de6f58ebe
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-13 09:45:47 +0800

    Initial Commit

---
+++ yoFB2019_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yoFB2019_swiftUI
+//
+//  Created by Yogi Arif Widodo on 13/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
